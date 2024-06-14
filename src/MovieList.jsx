ae0cd254e09429da0ab2445ead5796de3791a635
Mojolajesu Mercy Dada <mojolajesu@meta.com>
2024-06-14 14:24:18 -0700

    edited css features

--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -5,6 +5,23 @@ import './MovieList.css'
 const MovieList = ({movies, selectedSort}) => {
   const [sortedMovies, setSortedMovies] = useState([]);
 
+  //Tried to implement a getMovieDetails Fetch function
+// const getMovieDetails = async(movie_id) => {
+//   let url = `https://api.themoviedb.org/3/movie/${movie_id}?language=en-US&api_key=${API_KEY}`
+
+//   const options = {
+//     method: 'GET',
+//     headers: {
+//       accept: 'application/json',
+//       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0Y2NkNWViYjMzNzBlYjc4MzQxNWViZTYxMjFhOGYxNyIsInN1YiI6IjY2Njc4NTU3M2RlNTFiZmE0YzFjMmU2ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NBzva_Lu1kD0S-TVYFRe2TpeBbFuJnblRAPNLZUbbfE'
+//     }
+//   };
+
+//   const res = await fetch(url, options);
+//   const data = await res.json();
+//   setMovies((prevMovies) => page === 1 ? data.results : [...prevMovies, ...data.results]);
+// }
+
   useEffect(() => {
     const sortMovies = () => {
       let sorted = [...movies];
@@ -28,6 +45,7 @@ const MovieList = ({movies, selectedSort}) => {
             movieRating={movie.vote_average}
             movieReleaseDate={movie.release_date}
             movie = {movie}
+            // movieDetails = {getMovieDetails(movie.id)}
         />
       ))}
      </div>
